cd8f4c93c3a149a4cb9b9b7791a207b9bd47fd7d
Jérémi Gendron <gendronjeremi@gmail.com>
2020-05-08 19:44:07 -0400

    rename typescript.ts to utils.ts (its just utility functions) -- various undocumented improvements to error handling/prevention which were spotted by a nice tsconfig

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,30 @@
-import * as ts from 'typescript';
-import * as fse from 'fs-extra';
-import * as _ from 'lodash';
-import * as path from 'path';
+import {
+  CompilerOptions,
+  ScriptTarget,
+  ModuleResolutionKind,
+  createProgram,
+  getPreEmitDiagnostics,
+  sys,
+  parseJsonConfigFileContent,
+  parseConfigFileTextToJson,
+  flattenDiagnosticMessageText,
+} from "typescript";
+import * as fse from "fs-extra";
+import * as _ from "lodash";
+import * as path from "path";
 
-import { ServerlessTSFunction } from './types';
+import { ServerlessTSFunction } from "./types";
 
-export const makeDefaultTypescriptConfig = (): ts.CompilerOptions => {
-  const defaultTypescriptConfig: ts.CompilerOptions = {
+export const makeDefaultTypescriptConfig = (): CompilerOptions => {
+  const defaultTypescriptConfig: CompilerOptions = {
     preserveConstEnums: true,
     strictNullChecks: true,
     sourceMap: true,
     allowJs: true,
-    target: ts.ScriptTarget.ES5,
-    moduleResolution: ts.ModuleResolutionKind.NodeJs,
-    lib: ['lib.es2015.d.ts'],
-    rootDir: './',
+    target: ScriptTarget.ES5,
+    moduleResolution: ModuleResolutionKind.NodeJs,
+    lib: ["lib.es2015.d.ts"],
+    rootDir: "./",
   };
 
   return defaultTypescriptConfig;
@@ -31,24 +41,20 @@ export const extractFileNames = (
   // that we already read the tsconfig.json file, by inspecting the current
   // working directory. If the packageFile does not contain a valid main, then
   // it instead selects the index.js file.
-  if (provider === 'google') {
-    const packageFilePath = path.join(cwd, 'package.json');
+  if (provider === "google") {
+    const packageFilePath = path.join(cwd, "package.json");
     if (fse.existsSync(packageFilePath)) {
       // Load in the package.json file.
-      const packageFile = JSON.parse(
-        fse.readFileSync(packageFilePath).toString(),
-      );
+      const packageFile = JSON.parse(fse.readFileSync(packageFilePath).toString());
 
       // Either grab the package.json:main field, or use the index.ts file.
       // (This will be transpiled to index.js).
-      const main = packageFile.main
-        ? packageFile.main.replace(/\.js$/, '.ts')
-        : 'index.ts';
+      const main = packageFile.main ? packageFile.main.replace(/\.js$/, ".ts") : "index.ts";
 
       // Check that the file indeed exists.
       if (!fse.existsSync(path.join(cwd, main))) {
         console.log(`Cannot locate entrypoint, ${main} not found`);
-        throw new Error('Typescript compilation failed');
+        throw new Error("Typescript compilation failed");
       }
 
       return [main];
@@ -58,96 +64,75 @@ export const extractFileNames = (
   return _.values(functions)
     .map((fn) => fn.handler)
     .map((h) => {
-      const fnName = _.last(h.split('.'));
+      const fnName = _.last(h.split("."));
+      if (!fnName) throw new Error(`Couldn't get exported function name; missing name after '.' in ${h}`);
       const fnNameLastAppearanceIndex = h.lastIndexOf(fnName);
       // replace only last instance to allow the same name for file and handler
       const fileName = h.substring(0, fnNameLastAppearanceIndex);
 
       // Check if the .ts files exists. If so return that to watch
-      if (fse.existsSync(path.join(cwd, fileName + 'ts'))) {
-        return fileName + 'ts';
+      if (fse.existsSync(path.join(cwd, fileName + "ts"))) {
+        return fileName + "ts";
       }
 
       // Check if the .js files exists. If so return that to watch
-      if (fse.existsSync(path.join(cwd, fileName + 'js'))) {
-        return fileName + 'js';
+      if (fse.existsSync(path.join(cwd, fileName + "js"))) {
+        return fileName + "js";
       }
 
       // Can't find the files. Watch will have an exception anyway. So throw one with error.
-      console.log(`Cannot locate handler - ${fileName} not found`);
-      throw new Error(
-        'Typescript compilation failed. Please ensure handlers exists with ext .ts or .js',
-      );
+      console.log(`Cannot locate handler - ${fileName.slice(0, -1)} not found`);
+      throw new Error("Typescript compilation failed. Please ensure handlers exists with ext .ts or .js");
     });
 };
 
-export const run = (
-  fileNames: string[],
-  options: ts.CompilerOptions,
-): string[] => {
+export const run = (fileNames: string[], options: CompilerOptions): string[] | undefined => {
   options.listEmittedFiles = true;
-  const program = ts.createProgram(fileNames, options);
+  const program = createProgram(fileNames, options);
 
   const emitResult = program.emit();
 
-  const allDiagnostics = ts
-    .getPreEmitDiagnostics(program)
-    .concat(emitResult.diagnostics);
+  const allDiagnostics = getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
 
   allDiagnostics.forEach((diagnostic) => {
-    if (!diagnostic.file) {
-      console.log(diagnostic);
+    if (diagnostic.file && diagnostic.start) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+      const message = flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+      console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
     }
-    const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-      diagnostic.start,
-    );
-    const message = ts.flattenDiagnosticMessageText(
-      diagnostic.messageText,
-      '\n',
-    );
-    console.log(
-      `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`,
-    );
   });
 
   if (emitResult.emitSkipped) {
-    throw new Error('Typescript compilation failed');
+    throw new Error("Typescript compilation failed");
   }
 
-  return emitResult.emittedFiles.filter((filename) => filename.endsWith('.js'));
+  return emitResult.emittedFiles?.filter((filename) => filename.endsWith(".js"));
 };
 
 /*
  * based on rootFileNames returns list of all related (e.g. imported) source files
  */
-export const getSourceFiles = (
-  rootFileNames: string[],
-  options: ts.CompilerOptions,
-): string[] => {
-  const program = ts.createProgram(rootFileNames, options);
+export const getSourceFiles = (rootFileNames: string[], options: CompilerOptions): string[] => {
+  const program = createProgram(rootFileNames, options);
   const programmFiles = program.getSourceFiles().map((file) => file.fileName);
   return programmFiles;
 };
 
 export const getTypescriptConfig = (
   cwd: string,
-  tsconfigFilePath: string,
+  tsconfigFilePath = "tsconfig.json",
   logger?: { log: (str: string) => void },
-): ts.CompilerOptions => {
+): CompilerOptions => {
   const configFilePath = path.join(cwd, tsconfigFilePath);
 
   if (fse.existsSync(configFilePath)) {
     const configFileText = fse.readFileSync(configFilePath).toString();
-    const result = ts.parseConfigFileTextToJson(configFilePath, configFileText);
+    const result = parseConfigFileTextToJson(configFilePath, configFileText);
     if (result.error) {
       throw new Error(JSON.stringify(result.error));
     }
 
-    const configParseResult = ts.parseJsonConfigFileContent(
-      result.config,
-      ts.sys,
-      path.dirname(configFilePath),
-    );
+    const configParseResult = parseJsonConfigFileContent(result.config, sys, path.dirname(configFilePath));
     if (configParseResult.errors.length > 0) {
       throw new Error(JSON.stringify(configParseResult.errors));
     }
